626862651ff196305b6323c1fee8f90310c466dc
Shane Connon <shane.connon@gmail.com>
2023-02-12 14:53:41 +0000

    Release version: 0.0.5

---
+++ magic-odata-shared/README.md
@@ -0,0 +1,78 @@
+# magic-odata
+Magical OData client generation for typescript. No more `string`. No more `any`!
+
+## Install
+
+`npm i magic-odata-code-gen --save-dev; npm i magic-odata-client;`
+
+ * `magic-odata-code-gen` is a dev dependency required to generate a client at compile time
+     * This tool is small and we recommend installing it locally per project in order to avoid versioning issues
+ * `magic-odata-client` is a dependency of your application. The generated code will use this package
+
+## Generate
+
+`node node_modules/magic-odata-code-gen/dist/index.js --metadataUrl "https://raw.githubusercontent.com/ShaneGH/magic-odata/main/docs/sampleOdataMetadata.xml"`
+
+## Use
+
+```typescript
+// import a generated client
+import { ODataClient } from "./odataClient.js"
+
+// create a client instance
+const oDataClient = new ODataClient({
+    // inject a basic Http client
+    request: (input, init) => fetch(input, init),
+
+    // add a root URI
+    uriRoot: "https://my.odata.server/odata"
+})
+
+// use the client!
+const popularBlogPosts = oDataClient.BlogPosts
+    .withQuery((blogPost, {
+        $filter: {gt, or}, 
+        $orderby: {orderBy}, 
+        $skip, 
+        $top
+    }) => [ 
+        or(gt(blogPost.Comments.$count, 100), gt(blogPost.Likes, 100)),
+        orderBy(blogPost.Name),
+        $skip(0),
+        $top(10)
+    ])
+    .get();
+```
+
+## Why?
+
+Write safe, statically typed odata queries in typescript. No more `string`. No more `any`!
+
+ * Cut down on runtime errors
+ * Explore OData API possibilities in your IDE
+ * Runs in the browser or in node
+ * No prod dependencies. Small bundle size
+    * Generated executable code is tiny. Almost all generated code is `type` information
+    * `magic-odata-client` minifies to ~24KB
+ * Optional angular mode, for the angular `HttpClient`
+
+## Features
+
+ * Code gen configuration
+    * $metadata files protected with authentication
+    * Angular mode
+ * Client configuration
+ * Entity keys
+ * Entity path
+ * Query options
+    * $filter
+    * $select
+    * $expand
+    * $orderBy
+    * $search
+    * $skip, $top and $count
+    * custom
+ * Casting
+ * $value
+
+## Contributing
--- release/executeRelease.ps1
+++ release/executeRelease.ps1
@@ -12,83 +12,83 @@ echo ""
 echo "Step 1: create a release branch"
 $branch = "release/$version"
 $tag = "v$version"
-git checkout -b $branch
-if (-not($?)) {
-    exit 1
-}
+# git checkout -b $branch
+# if (-not($?)) {
+#     exit 1
+# }
 
-echo ""
-echo "Step 2: run tests"
-./executeTests.ps1 -outputLog $buildLog
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 2: run tests"
+# ./executeTests.ps1 -outputLog $buildLog
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo ""
-echo "Step 3: add correct versions"
-./reVersion.ps1 -version $version
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 3: add correct versions"
+# ./reVersion.ps1 -version $version
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo "Step 4: publish shared"
-./publish.ps1 -package ../magic-odata-shared
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo "Step 4: publish shared"
+# ./publish.ps1 -package ../magic-odata-shared
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo ""
-echo "Step 5: install versioned dependencies"
-./install.ps1 -package ../magic-odata-client
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 5: install versioned dependencies"
+# ./install.ps1 -package ../magic-odata-client
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-./install.ps1 -package ../magic-odata-code-gen
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# ./install.ps1 -package ../magic-odata-code-gen
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo ""
-echo "Step 6: run tests again with new versions"
-./executeTests.ps1 -outputLog $buildLog
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 6: run tests again with new versions"
+# ./executeTests.ps1 -outputLog $buildLog
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo ""
-echo "Step 7: publish client + code-gen"
-./publish.ps1 -package ../magic-odata-client
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 7: publish client + code-gen"
+# ./publish.ps1 -package ../magic-odata-client
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-./publish.ps1 -package ../magic-odata-code-gen
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# ./publish.ps1 -package ../magic-odata-code-gen
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-echo ""
-echo "Step 8: run tests a third time, pointing at actual npm packages as dependencies"
-./reVersion.ps1 -version $version -tests
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# echo ""
+# echo "Step 8: run tests a third time, pointing at actual npm packages as dependencies"
+# ./reVersion.ps1 -version $version -tests
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
-./executeTests.ps1 -outputLog $buildLog -npmInstall
-if (-not($?)) {
-    echo "Error with build. Suplimentary output logs: $buildLog"
-    exit 1
-}
+# ./executeTests.ps1 -outputLog $buildLog -npmInstall
+# if (-not($?)) {
+#     echo "Error with build. Suplimentary output logs: $buildLog"
+#     exit 1
+# }
 
 echo ""
 echo "Step 9: Commit release"
